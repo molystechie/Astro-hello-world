cda8ab58cb9fce029293a71825dc85dbb8b5ece9
johnnylyimo <johnnylyimo@gmail.com>
2022-08-19 04:33:56 +0300

    Updated codes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 module.exports = {
-    content: [
-      './public/**/*.html',
-      './src/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
-      '../../libs/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
-    ],
+    // content: [
+    //   './public/**/*.html',
+    //   './src/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
+    //   '../../libs/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
+    // ],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {},
   };
